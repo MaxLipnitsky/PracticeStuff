23de196c2cc6fd56bc63a269a3170acc40ba21b4
Max Lipnitsky <max.lipnitsky@mtdproducts.com>
2021-04-30 18:09:30 +0300

    Added foreground icon and WordDaoTest file.

---
+++ ArchitecturePractice/RoomWordSample/app/src/androidTest/java/com/maxl/roomwordsample/WordDaoTest.kt
@@ -0,0 +1,10 @@
+package com.maxl.roomwordsample
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+class WordDaoTest {
+
+}
